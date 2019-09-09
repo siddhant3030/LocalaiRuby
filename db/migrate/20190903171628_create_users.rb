31456d777f5276f1c697bfa143f6ce87b9b84c09
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-10 05:01:01 +0530

    added more field into migration

--- db/migrate/20190903171628_create_users.rb
+++ db/migrate/20190903171628_create_users.rb
@@ -3,7 +3,20 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.integer :vechile_id
-
+      t.integer :package_id
+      t.integer :travel_type_id
+      t.integer :from_area_id
+      t.integer :to_area_id
+      t.integer :to_city_id
+      t.string :online_booking
+      t.string :mobile_site_booking
+      t.integer :booking_created
+      t.integer :from_lat
+      t.integer :from_long
+      t.integer :to_let
+      t.integer :to_long
+      t.string :car_cancellation 
+      
       t.timestamps
     end
   end
